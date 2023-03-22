b9cd097cb981ca86427ec16fcc7dbfc177c9737c
Roy Morillo <rmorillor@gmail.com>
2023-03-22 13:35:03 -0400

    Shopping cart UI Ready

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,60 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            this._productService = productService;
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
+        public async Task<IActionResult> Remove(int cartDetailId)
+        {
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _cartService.RemoveToCartAsync<ResponseDTO>(cartDetailId, accessToken);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
+        private async Task<CartDTO> LoadCartDtoBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _cartService.GetCartByUserIdAsync<ResponseDTO>(userId, accessToken);
+
+            CartDTO cartDTO = new();
+            if (response!=null && response.IsSuccess)
+            {
+                cartDTO = JsonConvert.DeserializeObject<CartDTO>(Convert.ToString(response.Result));
+            }
+
+            if (cartDTO.CartHeader != null)
+            {
+                foreach (var detail in cartDTO.CartDetails)
+                {
+                    cartDTO.CartHeader.OrderTotal += (detail.Product.Price * detail.Count);
+                }                
+            }
+            return cartDTO;
+        }
+    }
+}
